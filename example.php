6f61bbe19b34176c2c2d179c2f7f214add887fa6
Alfred Francis <alfredfrancis@users.noreply.github.com>
2014-11-29 20:57:00 +0530

    Create example.php

---
+++ example.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Script to convert info Hash to Magnetic URI
+ * @author Alfred francis
+ * @link http://prowd.in
+ */
+include_once "alfa.hash2mui.class.php";
+$h2m=new Hash2mui();
+echo $h2m->grab_mui("11A2AC68A11634E980F265CB1433C599D017A759");
+?>
